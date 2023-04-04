6d483e320bcf7a479f0ae4f99f347117e836232f
eD3ath <edeath1208@gmail.com>
2023-04-04 20:00:32 +0800

    email template

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -2,9 +2,13 @@ import { Component, Inject, OnInit, SecurityContext } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import html2canvas from 'html2canvas';
+import Swal from 'sweetalert2';
+
+import { EmailService } from 'src/app/services/email.service';
 
 interface URL {
   name: string;
+  email: string;
   url: SafeUrl;
   kidIndex: number
 }
@@ -19,7 +23,7 @@ export class QrcodeComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<QrcodeComponent>,
     @Inject(MAT_DIALOG_DATA) public qrData: any,
-    private sanitizer: DomSanitizer
+    public emailService: EmailService
   ) { }
 
   ngOnInit(): void {
@@ -30,7 +34,7 @@ export class QrcodeComponent implements OnInit {
       id,
       kidIndex,
       name
-    })
+    });
   }
 
   onChangeURL(url: SafeUrl, name: string, kidIndex: number) {
@@ -38,22 +42,20 @@ export class QrcodeComponent implements OnInit {
       name,
       url,
       kidIndex
-    } as URL)
+    } as URL);
   }
 
-  async downloadQRs() {
-    await Promise.all(this.urls.map(async (data: URL) => {
+  async genTemplate() {
+    const qrImages = await Promise.all(this.urls.map(async (data: URL) => {
       const img: any = document.querySelector(`#card${data.kidIndex + 1}`);
-      const canvas = await html2canvas(img)
-      const textToSaveAsURL = canvas.toDataURL("image/png");
-      const downloadLink = document.createElement('a');
-      downloadLink.download = `${data.name}.png`;
-      downloadLink.innerHTML = 'Download File';
-      downloadLink.href = textToSaveAsURL;
-      downloadLink.style.display = 'none';
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-    }))
+      const canvas = await html2canvas(img, { backgroundColor: null });
+      return `<img src='${canvas.toDataURL("image/png")}'>`;
+    }));
+    Swal.fire({
+      title: 'Email Template',
+      html: `<p>${this.qrData.email}</p><hr>${this.emailService.getTemplate(this.qrData.kids)}<hr>${qrImages.join('<br>')}`,
+      focusConfirm: false
+    });
   }
 
 }
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  emailTemplate = ''
+  subject = ''
+  constructor() {
+    this.subject = `Camp Cesaria QR Code`
+    this.emailTemplate = `<div class="align-left">
+      <p>Dear Guest/s,</p>
+      <p>Thank you for registering your {{var1}}, {{names}}, in Camp Cesaria's First Easter Egg Hunt Activity this coming Sunday, April 9, 2023.</p>
+      <p>Please see the attachment for your {{var2}} QR {{qr}} that should be presented at the entrance and food kiosks.</p>
+      <p>Please note:</p>
+      <p>
+      <ul>
+        <li>P500 registration fee is inclusive of entrance to Camp Cesaria, Taters Popcorn, Egg Sandwich by K-Egg Kidapawan,
+          pool access, entrance to the rabbit cove, face painting, bubble show, balloon twisting, easter egg hunting, and
+          more games and prizes.</li>
+        <li>Every adult has to pay P100 for the entrance, inclusive of pool access only.</li>
+        <li>Please bring a floater or life vest for kids if you have any.</li>
+        <li>Adults are NOT allowed to pick up easter eggs during the easter egg hunting.</li>
+      </ul>
+      </p>
+      <p>If you have further questions, don't hesitate to contact us through our facebook page: <a
+          href="https://www.facebook.com/campcesaria" target="_blank">Camp Cesaria</a></p>
+    </div>`
+  }
+
+  getTemplate(names: string[]) {
+    return this.emailTemplate.replace('{{var1}}', names.length > 1 ? 'kids' : 'kid').replace('{{names}}', names.join(', ')).replace('{{var2}}', names.length > 1 ? 'kids\'' : 'kid\'s').replace('{{qr}}', names.length > 1 ? 'codes' : 'code')
+  }
+
+  getMailto(names: string[], email: string) {
+    const body = this.emailTemplate.replace('{{var1}}', names.length > 1 ? 'kids' : 'kid').replace('{{names}}', names.join(', ')).replace('{{var2}}', names.length > 1 ? 'kids\'' : 'kid\'s').replace('{{qr}}', names.length > 1 ? 'codes' : 'code')
+    return `mailto:${email}?subject=${this.subject}&body=${body}`
+  }
+}
