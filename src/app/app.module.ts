ccc03e6159108ea5a2693c3225e45591a744535b
eD3ath <edeath1208@gmail.com>
2023-04-08 14:42:52 +0800

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { ClaimComponent } from './claim/claim.component';
 import { QrcodeComponent } from './qrcode/qrcode.component';
 import { LandingComponent } from './landing/landing.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -37,6 +39,12 @@ import { LayoutComponent } from './layout/layout.component';
     MaterialModule,
     BrowserAnimationsModule,
     LayoutModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
